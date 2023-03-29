f698aa523f8833e451b3533c5d3c4ff57496ad00
juan5897230 <125592019+juan5897230@users.noreply.github.com>
2023-03-29 14:21:37 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+  <head>
+    <title>Datos</title>
+  </head>
+  <body>
+    <?php
+       $nombre = "Juan Francisco Valdez Zuñiga";
+       $calle = "rosal";
+       $numero = "653";
+       $colonia = "Valle de las flores";
+       $municipio = "Saltillo coahuila";
+       $telefono = "844 1739274";
+
+       echo "Nombre completo: $nombre  ";
+        ?>
+<br>
+<?php
+       echo 
+    " Calle donde vivo: $calle ";
+        ?>
+<br>
+<?php
+       echo "Colonia en la que vivo: $colonia ";
+        ?>
+<br>
+<?php
+       echo " Numero de la casa: $numero ";
+        ?>
+<br>
+<?php
+       echo "  Municipio : $municipio  ";
+       ?>
+<br>
+<?php
+       echo " Mi numero de telefono es:$telefono";
+    ?>
+  </body>
+</html>
